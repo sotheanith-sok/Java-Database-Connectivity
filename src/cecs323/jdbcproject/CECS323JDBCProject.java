d0592ce6d3794db2bf38875dc9c04eba9cd1b6a6
Sotheanith Sok <thetheanith@gmail.com>
2017-03-07 12:23:03 -0800

    Starting

---
+++ src/cecs323/jdbcproject/CECS323JDBCProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cecs323.jdbcproject;
+
+/**
+ *
+ * @author Sotheanith
+ */
+public class CECS323JDBCProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
